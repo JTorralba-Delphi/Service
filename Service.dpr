a05d782170483c859eabecb23fb24bffdafc87f3
Julius Torralba <Julius.Torralba@GMail.com>
2022-05-14 23:01:25 -0600

    Log syntax error(s).

--- Service.dpr
+++ Service.dpr
@@ -1,6 +1,7 @@
 program Service;
 
 uses
+
   System.SysUtils,
 
   VCL.Forms,
@@ -21,12 +22,16 @@ begin
       VCL.Forms.Application.CreateForm(TDelphi_GUI, Delphi_GUI);
       VCL.Forms.Application.Run;
     end
-  else
+  else if FindCmdLineSwitch('INSTALL', ['/'], True) or FindCmdLineSwitch('UNINSTALL', ['/'], True) then
     begin
       if not VCL.SvcMgr.Application.DelayInitialize or VCL.SvcMgr.Application.Installing then VCL.SvcMgr.Application.Initialize;
       VCL.SvcMgr.Application.CreateForm(TDelphi, Delphi);
       Delphi.ImagePath := GetImagePath;
       VCL.SvcMgr.Application.Run;
+    end
+    else
+    begin
+      Log('Service', 'Invalid argument(s).');
     end;
 end.
 
